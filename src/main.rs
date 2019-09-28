25bb876461c41d01998217844cbbacf35e24e659
notrodes <reallystupidid@gmail.com@gmail.com>
2019-09-27 21:19:53 -0400

    Test code/demo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-use gtk;
-fn main() {
+use std::env;
 
+use gio::prelude::*;
+use gtk::prelude::*;
+
+fn main() {
+    let uiapp = gtk::Application::new(None, gio::ApplicationFlags::FLAGS_NONE)
+        .expect("Application::new failed");
+    uiapp.connect_activate(|app| {
+        let win = gtk::ApplicationWindow::new(app);
+        win.set_default_size(320, 200);
+        win.set_title("Basic example");
+        win.show_all();
+    });
+    uiapp.run(&env::args().collect::<Vec<_>>());
 }
